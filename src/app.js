eecd07515eaadd527512b3f12b39daa9ff1b6f0f
Théo LAVAUX <theo.lavaux@epitech.eu>
2018-11-19 11:34:59 +0700

    Initializing the repository

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(
+  <p>Here is an awesome cooking webapp!</p>,
+  document.getElementById('app')
+);

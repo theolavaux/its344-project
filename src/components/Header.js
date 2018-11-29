ed47ef3b1d0ee3ca0fcf22ac9ece15e9bca3c5ed
Théo LAVAUX <theo.lavaux@epitech.eu>
2018-11-29 10:54:50 +0700

    Changed name

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => (
     <div className="content-container">
       <div className="header__content">
         <Link className="header__title" to="/">
-          <h1>Cookify</h1>
+          <h1>Easy meal</h1>
         </Link>
       </div>
     </div>
